e48eb3d5faf89b2e8410b238df27c9672304b488
Ben Thompson <t.ben.thompson@gmail.com>
2017-01-01 16:58:54 -0500

    Moved from tectosaur to here.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    packages = ['tectosaur_tables'],
+    install_requires = [],
+    zip_safe = False,
+
+    name = 'tectosaur_tables',
+    version = '0.0.1',
+    description = '',
+    author = 'T. Ben Thompson',
+    author_email = 't.ben.thompson@gmail.com',
+    license = 'GPL',
+    platforms = ['any']
+    # Add classifiers
+)
