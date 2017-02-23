e1a573f9bf536527c1762ae4785f066a839eb5ce
Ben Thompson <t.ben.thompson@gmail.com>
2017-02-23 11:39:02 -0500

    2d?

--- conv_tests/adj_conv.py
+++ conv_tests/adj_conv.py
@@ -54,60 +54,60 @@ def rateval_with_log(x, coeffs):
 
 data = [4.808509588401806e-07, 2.859074232873816e-06, 1.1925641395873547e-05, 3.988211942814987e-05, 0.00011449083571240847, 0.0002934196140107116, 0.0006827105644366299, 0.0014456945537162772, 0.00278183480335222, 0.004875127130823552, 0.007844460727703402, 0.011726055058313504, 0.016486447672913615, 0.022047431313778138, 0.02830902562671784, 0.03516577759095902, 0.04251670750169806, 0.050270601813516946, 0.05834825447763689, 0.0666828360941662, 0.07521919274840261, 0.08391260074644313, 0.09272731288525342]
 eps = [0.8, 0.565685424949238, 0.39999999999999997, 0.28284271247461895, 0.19999999999999996, 0.14142135623730945, 0.09999999999999996, 0.07071067811865472, 0.049999999999999975, 0.035355339059327355, 0.024999999999999984, 0.017677669529663678, 0.01249999999999999, 0.008838834764831837, 0.006249999999999994, 0.004419417382415918, 0.0031249999999999967, 0.0022097086912079584, 0.0015624999999999981, 0.001104854345603979, 0.000781249999999999, 0.0005524271728019895, 0.0003906249999999994]
+data = [0.0006827105644366296, 0.0010053619592424034, 0.0014456945537162757, 0.0020295631534195035, 0.002781834803352217, 0.0037244876921632137, 0.004875127130823547, 0.006246133353873914, 0.007844460727703393, 0.009671962654106145, 0.011726055058313487, 0.014000539146491946, 0.01648644767291359, 0.0191728297181584, 0.02204743131377812, 0.025097258384754736, 0.028309025626717815, 0.03166950338356974, 0.035165777590959, 0.03878543787218614, 0.04251670750169802, 0.04634852707388779, 0.0502706018135169, 0.05427342074838063, 0.05834825447763686, 0.062487137009825826, 0.06668283609416616, 0.0709288155966472, 0.07521919274840257, 0.07954869249518562, 0.08391260074644308, 0.08830671792571852, 0.09272731288525335, 0.09717107883321792, 0.10163509537829399, 0.10611677806974795, 0.11061386459556148, 0.11512435342862974, 0.11964651389583734, 0.12417877900192174]
+eps = [0.1, 0.08408964152537146, 0.07071067811865477, 0.059460355750136064, 0.05000000000000002, 0.04204482076268574, 0.03535533905932738, 0.029730177875068032, 0.02500000000000001, 0.021022410381342872, 0.017677669529663695, 0.01486508893753402, 0.012500000000000006, 0.010511205190671438, 0.00883883476483185, 0.007432544468767011, 0.006250000000000003, 0.00525560259533572, 0.004419417382415925, 0.003716272234383506, 0.0031250000000000023, 0.00262780129766786, 0.0022097086912079627, 0.001858136117191753, 0.0015625000000000014, 0.0013139006488339302, 0.0011048543456039816, 0.0009290680585958768, 0.0007812500000000007, 0.0006569503244169652, 0.0005524271728019908, 0.00046453402929793843, 0.00039062500000000046, 0.00032847516220848265, 0.00027621358640099545, 0.00023226701464896922, 0.00019531250000000023, 0.00016423758110424135, 0.00013810679320049775, 0.00011613350732448464]
 data = np.array(data)
 eps = np.array(eps)
-def main():
-    f = lambda x: (0.2 - x / 10.0) / (1.0 + x / 10.0) + 3.0 * np.log(x)
-    fvs = f(eps)
-    ratinterp_with_log(eps, fvs, 5)
-
-    first = 10
-    last = 18
-    step = 1
-    test_first = first
-    test_last = last + 4
-    for i in range(1, 6):
-        I = ratinterp_with_log(eps[first:last], data[first:last], i)
-        I_data = rateval_with_log(eps[test_first:test_last], I)
-        print(
-            "rational interp order=" + str(i) + ": " +
-            str(np.max(np.abs(data[test_first:test_last] - I_data)))
-        )
-
 # def main():
-#     lim = -0.11656244
-#     factor = -0.02652583
-#     # data = data - factor * np.log(eps)
+#     f = lambda x: (0.2 - x / 10.0) / (1.0 + x / 10.0) + 3.0 * np.log(x)
+#     fvs = f(eps)
+#     ratinterp_with_log(eps, fvs, 5)
 #
 #     first = 10
 #     last = 18
 #     step = 1
 #     test_first = first
 #     test_last = last + 4
-#     print(8e-1 / (np.sqrt(2) ** 10), eps[last - 1])
-#
-#     prev_val = None
-#     for i in range(last - first - 1):
-#         I_old = limit_coeffs(eps[first:last:step], data[first:last:step], i)
-#         I_old_data = limit_interp_eval(eps[test_first:test_last], *I_old)
-#         I_limit = I_old[0][i]
-#         # print(I_limit)
-#         if prev_val is not None:
-#             print(I_limit, np.abs((I_limit - prev_val) / I_limit))
-#         prev_val = I_limit
-#         print("poly+log" + str(i) + " interp: " + str(np.max(np.abs(data[test_first:test_last] - I_old_data))))
-#
-#     for i in range(2, 12):
-#         I = ratinterp(np.log10(eps)[first:last:step], data[first:last:step], i)
-#         I_data = rateval(np.log10(eps[test_first:test_last]), I)
+#     for i in range(1, 6):
+#         I = ratinterp_with_log(eps[first:last], data[first:last], i)
+#         I_data = rateval_with_log(eps[test_first:test_last], I)
 #         print(
 #             "rational interp order=" + str(i) + ": " +
 #             str(np.max(np.abs(data[test_first:test_last] - I_data)))
 #         )
-        # plt.plot(np.log10(eps[test_first:test_last]), data[test_first:test_last], 'b-')
-        # plt.plot(np.log10(eps[test_first:test_last]), I_data, 'r-')
-        # plt.plot(np.log10(eps[test_first:test_last]), I_old_data, 'k-')
-        # plt.show()
+
+def main():
+    # data = data - factor * np.log(eps)
+
+    first = 14
+    prev_val = None
+    for last in range(15, 30):
+        step = 1
+        test_first = first
+        test_last = last + 4
+
+        # for i in range(last - first - 1):
+        log_terms = (last - first - 1) // 2
+        I_old = limit_coeffs(eps[first:last:step], data[first:last:step], log_terms)
+        I_old_data = limit_interp_eval(eps[test_first:test_last], *I_old)
+        I_limit = I_old[0][log_terms]
+        # print(I_limit)
+        if prev_val is not None:
+            print(last, I_limit, np.abs((I_limit - prev_val) / I_limit))
+        prev_val = I_limit
+        # print("poly+log" + str(last) + " interp: " + str(np.max(np.abs(data[test_first:test_last] - I_old_data))))
+
+    # for i in range(2, 12):
+    #     I = ratinterp(np.log10(eps)[first:last:step], data[first:last:step], i)
+    #     I_data = rateval(np.log10(eps[test_first:test_last]), I)
+    #     print(
+    #         "rational interp order=" + str(i) + ": " +
+    #         str(np.max(np.abs(data[test_first:test_last] - I_data)))
+    #     )
+    #     plt.plot(np.log10(eps[test_first:test_last]), data[test_first:test_last], 'b-')
+    #     plt.plot(np.log10(eps[test_first:test_last]), I_data, 'r-')
+    #     plt.plot(np.log10(eps[test_first:test_last]), I_old_data, 'k-')
+    #     plt.show()
 
     # plt.plot(eps, data)
     # plt.show()
---
+++ tests/test_2d.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+from tectosaur.limit import limit
+from tectosaur.geometry import element_pt
+
+import cppimport
+adaptive_integrate = cppimport.imp('tectosaur_tables.adaptive_integrate').adaptive_integrate
+
+def unscaled_seg_normal(corners):
+    return np.array([-corners[1][1] + corners[0][1], corners[1][0] - corners[0][0]])
+
+def normalize(vec):
+    return vec / np.linalg.norm(vec)
+
+def linear_basis_seg(xhat):
+    return [1 - xhat, xhat]
+
+def make_2d_integrator(obs_el, src_el, eps):
+    obs_el = np.array(obs_el)
+    src_el = np.array(src_el)
+    obs_nx, obs_ny = unscaled_seg_normal(obs_el)
+    src_nx, src_ny = unscaled_seg_normal(src_el)
+    eps_offset_x, eps_offset_y = normalize([obs_nx, obs_ny]) * eps
+    def f(x):
+        obs_xhat = x[:,0]
+        src_xhat = x[:,1]
+
+        obs_basis = linear_basis_seg(obs_xhat)
+        obs_pt = element_pt(obs_basis, obs_el)
+        xx = obs_pt[0] + eps_offset_x
+        xy = obs_pt[1] + eps_offset_y
+
+        src_basis = linear_basis_seg(src_xhat)
+        src_pt = element_pt(src_basis, src_el)
+        yx = src_pt[0]
+        yy = src_pt[1]
+
+        rx = xx - yx
+        ry = xy - yy
+        r2 = rx ** 2 + ry ** 2
+
+        dotnm = obs_nx * src_nx + obs_ny * src_ny
+        dotrn = obs_nx * rx + obs_ny * ry
+        dotrm = src_nx * rx + src_ny * ry
+
+        # Single layer
+        kernel = np.log(np.sqrt(r2)) / (2 * np.pi)
+        # Double layer
+        # kernel = dotrm / (2 * np.pi * r2)
+        # Hypersingular
+        # kernel = ((-dotnm / r2) + (2 * dotrn * dotrm) / (r2 * r2)) / (2 * np.pi)
+
+        out = np.empty((4, kernel.shape[0]))
+        for i in range(2):
+            for j in range(2):
+                out[i * 2 + j] = obs_basis[i] * src_basis[j] * kernel
+        return out.T
+    return f
+
+def laplace_integral(el, eps = 0.1):
+    tol = 1e-4
+    est = adaptive_integrate.integrate(
+        make_2d_integrator(el, el, eps), [0, 0], [1, 1], tol
+    )[0]
+    return est
+
+def laplace_tester(el, correct):
+    np.testing.assert_almost_equal(laplace_integral(el), correct)
+
+def test_2d_laplace_simplest():
+    laplace_tester(
+        [[0, 0], [1, 0]],
+        [0.291354746782188, 0.0759048754363768, 0.075904875436377, 0.291354746782187]
+    )
+
+def test_2d_laplace_double_length():
+    laplace_tester(
+        [[0, 0], [2, 0]],
+        [0.398599297267922, 0.0783850214314389, 0.0783850214314391, 0.398599297267922]
+    )
+
+def test_2d_laplace_rotated():
+    laplace_tester(
+        [[0, 0], [1, 1]],
+        [0.34455624305342, 0.0774673541686122, 0.0774673541686119, 0.344556243053421]
+    )
+
+def test_2d_laplace_translated():
+    laplace_tester(
+        [[1, 1], [1, 2]],
+        [0.291354746782187, 0.0759048754363769, 0.0759048754363769, 0.291354746782187]
+    )
+
+def test_2d_rotate():
+    epsvs = 0.05 * (2.0 ** (-np.arange(6)))
+    print(epsvs)
+    theta = 0.0
+    for scale in (2.0 ** np.arange(-1, 2)):
+        vals = []
+        for eps in epsvs:
+            vals.append(laplace_integral(
+                [[0, 0], [np.cos(theta) * scale, np.sin(theta) * scale]],
+                eps = eps * scale
+            ))
+        print(epsvs, vals)
+        lim = limit(epsvs, vals, 1, False)
+        print('')
+        print(lim)
+    # for theta in np.linspace(0, 2 * np.pi, 100)[:-1]:
+    #     print(laplace_integral([[0, 0], [np.cos(theta), np.sin(theta)]]))
+
